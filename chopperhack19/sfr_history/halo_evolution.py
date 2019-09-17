22392a89c55610176feb5d819faf5839e244f4c3
Andrew Hearin <ahearin@anl.gov>
2019-09-17 10:00:08 -0500

    Changed halo_evolution module to numba-based implementation

--- chopperhack19/sfr_history/halo_evolution.py
+++ chopperhack19/sfr_history/halo_evolution.py
@@ -1,33 +1,57 @@
 """Starting from a halo mass at z=0, the two functions below give descriptions for
 how halo mass and Vmax smoothly evolve across time.
 """
-import numpy as np
+from numba import njit
+from math import log10, exp
 
 
 __all__ = ('halo_mass_vs_redshift', 'vmax_vs_mhalo_and_redshift')
 
 
-def halo_mass_vs_redshift(halo_mass_at_z0, redshift):
+@njit
+def halo_mass_vs_redshift(halo_mass_at_z0, redshift, halo_mass_at_z):
     """Fitting function from Behroozi+13, https://arxiv.org/abs/1207.6105,
-    Equations (H2)-(H6). Calibration assumes h=0.7.
+    Equations (H2)-(H6).
 
     Parameters
     ----------
     halo_mass_at_z0 : float or ndarray
-        Mass of the halo at z=0 assuming h=0.7.
+        Mass of the halo at z=0 assuming h=0.7
 
     redshift : float or ndarray
 
-    Returns
-    -------
-    halo_mass : ndarray
-        Mass of the halo at the input redshift assuming h=0.7.
+    halo_mass_at_z : ndarray
+        Empty array that will be filled with halo mass at the input redshift
     """
-    M0, z = _get_1d_arrays(halo_mass_at_z0, redshift)
-    return _M13(z)*10**_f(M0, z)
+    n = halo_mass_at_z.size
+
+    for i in range(n):
+        m_z0 = halo_mass_at_z0[i]
+        z = redshift[i]
+        a = 1./(1. + z)
+
+        _M13_z0 = 10**13.276
+
+        _M13_zfactor1 = (1. + z)**3.0
+        _M13_zfactor2 = (1. + 0.5*z)**-6.11
+        _M13_zfactor3 = exp(-0.503*z)
+        _M13 = _M13_z0*_M13_zfactor1*_M13_zfactor2*_M13_zfactor3
+
+        _exparg_factor1 = log10(m_z0/_M13_z0)
+
+        logarg = ((10**9.649)/m_z0)**0.18
+        a0 = 0.205 - log10(logarg + 1.)
+
+        _factor2_num = 1. + exp(-4.651*(1-a0))
+        _factor2_denom = 1. + exp(-4.651*(a-a0))
+        _exparg_factor2 = _factor2_num/_factor2_denom
+        _exparg = _exparg_factor1*_exparg_factor2
+
+        halo_mass_at_z[i] = _M13*(10**_exparg)
 
 
-def vmax_vs_mhalo_and_redshift(mhalo, redshift):
+@njit
+def vmax_vs_mhalo_and_redshift(mhalo, redshift, vmax):
     """Scaling relation between Vmax and Mhalo for host halos across redshift.
 
     Relation taken from Equation (E2) from Behroozi+19,
@@ -36,55 +60,21 @@ def vmax_vs_mhalo_and_redshift(mhalo, redshift):
     Parameters
     ----------
     mhalo : float or ndarray
-        Mass of the halo at the input redshift assuming h=0.7.
+        Mass of the halo at the input redshift assuming h=0.7
 
     redshift : float or ndarray
 
-    Returns
-    -------
     vmax : ndarray
-        Vmax [physical km/s] for a halo of the input mass at the input redshift
+        Empty array that will be filled with Vmax [physical km/s]
+        for a halo of the input mass and redshift
     """
-    mhalo, z = _get_1d_arrays(mhalo, redshift)
-    a = 1./(1. + z)
+    n = vmax.size
+    for i in range(n):
+        m = mhalo[i]
+        z = redshift[i]
+        a = 1/(1 + z)
 
-    denom_term1 = (a/0.378)**-0.142
-    denom_term2 = (a/0.378)**-1.79
-    numerator = 1.64e12
-    mpivot = numerator/(denom_term1 + denom_term2)
-    return 200.*(mhalo/mpivot)**(1/3.)
-
-
-def _M13(z):
-    factor1 = 10**13.276
-    factor2 = (1. + z)**3.0
-    factor3 = (1. + 0.5*z)**-6.11
-    factor4 = np.exp(-0.503*z)
-    return factor1*factor2*factor3*factor4
-
-
-def _f(M0, z):
-    factor1 = np.log10(M0/_M13(0.))
-    factor2_num = _g(M0, 1.)
-    factor2_denom = _g(M0, 1./(1.+z))
-    factor2 = factor2_num/factor2_denom
-    return factor1*factor2
-
-
-def _g(M0, a):
-    a0 = _get_a0(M0)
-    return 1. + np.exp(-4.651*(a-a0))
-
-
-def _get_a0(M0):
-    logarg = ((10**9.649)/M0)**0.18
-    return 0.205 - np.log10(logarg + 1.)
-
-
-def _get_1d_arrays(*args):
-    results = [np.atleast_1d(arg) for arg in args]
-    sizes = [arr.size for arr in results]
-    npts = max(sizes)
-    msg = "All input arguments should be either a float or ndarray of shape ({0}, )"
-    assert set(sizes) <= set((1, npts)), msg.format(npts)
-    return [np.zeros(npts).astype(arr.dtype) + arr for arr in results]
+        denom_term1 = (a/0.378)**-0.142
+        denom_term2 = (a/0.378)**-1.79
+        mpivot = 1.64e12/(denom_term1 + denom_term2)
+        vmax[i] = 200*(m/mpivot)**(1/3.)
