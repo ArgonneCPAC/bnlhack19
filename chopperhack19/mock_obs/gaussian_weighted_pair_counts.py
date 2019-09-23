5e0ef743b94f3351de86611f71817189edf692b2
Matthew R Becker <becker.mr@gmail.com>
2019-09-23 16:35:51 -0400

    try this

--- chopperhack19/mock_obs/gaussian_weighted_pair_counts.py
+++ chopperhack19/mock_obs/gaussian_weighted_pair_counts.py
@@ -2,7 +2,10 @@
 """
 from numba import cuda, njit, prange
 
-__all__ = ('count_weighted_pairs_3d_cuda', 'count_weighted_pairs_3d_cpu')
+__all__ = (
+    'count_weighted_pairs_3d_cuda',
+    'count_weighted_pairs_3d_cpu',
+    'count_weighted_pairs_3d_cpu_serial')
 
 
 @cuda.jit
@@ -54,6 +57,37 @@ def count_weighted_pairs_3d_cpu(x1, y1, z1, w1, x2, y2, z2, w2, rbins_squared, r
         pz = z1[i]
         pw = w1[i]
         for j in prange(n2):
+            qx = x2[j]
+            qy = y2[j]
+            qz = z2[j]
+            qw = w2[j]
+            dx = px-qx
+            dy = py-qy
+            dz = pz-qz
+            wprod = pw*qw
+            dsq = dx*dx + dy*dy + dz*dz
+
+            k = nbins-1
+            while dsq <= rbins_squared[k]:
+                result[k-1] += wprod
+                k=k-1
+                if k<=0:
+                    break
+
+
+@njit()
+def count_weighted_pairs_3d_cpu_serial(x1, y1, z1, w1, x2, y2, z2, w2, rbins_squared, result):
+
+    n1 = x1.shape[0]
+    n2 = x2.shape[0]
+    nbins = rbins_squared.shape[0]
+
+    for i in range(n1):
+        px = x1[i]
+        py = y1[i]
+        pz = z1[i]
+        pw = w1[i]
+        for j in range(n2):
             qx = x2[j]
             qy = y2[j]
             qz = z2[j]
