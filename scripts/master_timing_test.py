61e4580a31b5d10a327af81cc44ced1b03bd8a1c
Matthew R Becker <becker.mr@gmail.com>
2019-09-23 15:20:11 -0400

    bugz

--- scripts/master_timing_test.py
+++ scripts/master_timing_test.py
@@ -15,6 +15,9 @@ func = getattr(chopperhack19.mock_obs, func_str)
 if func is None:
     raise ImportError('could not import %s' % func_str)
 
+print('func_str:', func_str)
+print('func:', func)
+
 Lbox = 1000.
 result = np.zeros_like(DEFAULT_RBINS_SQUARED)
 
@@ -31,11 +34,53 @@ n2 = 50013
 x1, y1, z1, w1 = random_weighted_points(n1, Lbox, 0)
 x2, y2, z2, w2 = random_weighted_points(n2, Lbox, 1)
 
-start = time()
-func(
-    x1, y1, z1, w1, x2, y2, z2, w2, DEFAULT_RBINS_SQUARED, result)
-end = time()
-runtime = end-start
+if 'cuda' in func_str:
+    from numba import cuda
 
-print('func:', func_str)
+    d_x1 = cuda.to_device(x1)
+    d_y1 = cuda.to_device(y1)
+    d_z1 = cuda.to_device(z1)
+    d_w1 = cuda.to_device(w1)
+
+    d_x2 = cuda.to_device(x2)
+    d_y2 = cuda.to_device(y2)
+    d_z2 = cuda.to_device(z2)
+    d_w2 = cuda.to_device(w2)
+
+    d_rbins_squared = cuda.to_device(DEFAULT_RBINS_SQUARED)
+    d_result = cuda.device_array_like(result)
+
+    threads_per_warp = 32
+    warps_per_block = 4
+    threads_per_block = warps_per_block*threads_per_warp
+    blocks_per_grid = 32
+
+    start = time()
+    func[blocks_per_grid, threads_per_block](
+        d_x1, d_y1, d_z1, d_w1, d_x2, d_y2, d_z2, d_w2,
+        d_rbins_squared, d_result)
+    results_host = d_result.copy_to_host()
+    end = time()
+
+else:
+    d_x1 = x1
+    d_y1 = y1
+    d_z1 = z1
+    d_w1 = w1
+
+    d_x2 = x2
+    d_y2 = y2
+    d_z2 = z2
+    d_w2 = w2
+
+    d_rbins_squared = DEFAULT_RBINS_SQUARED
+    d_result = result
+
+    start = time()
+    func(
+        d_x1, d_y1, d_z1, d_w1, d_x2, d_y2, d_z2, d_w2,
+        d_rbins_squared, d_result)
+    end = time()
+    runtime = end-start
+
 print('time:', runtime)
