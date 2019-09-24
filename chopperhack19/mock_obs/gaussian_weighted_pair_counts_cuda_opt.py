9561ea06d52aa91869c45f1ef3f80b720462a714
Matthew R Becker <becker.mr@gmail.com>
2019-09-24 10:19:06 -0400

    do this

--- chopperhack19/mock_obs/gaussian_weighted_pair_counts_cuda_opt.py
+++ chopperhack19/mock_obs/gaussian_weighted_pair_counts_cuda_opt.py
@@ -5,7 +5,7 @@ import math
 __all__ = ('count_weighted_pairs_3d_cuda_smem',)
 
 
-@cuda.jit(fastmath=True)
+@cuda.jit()
 def count_weighted_pairs_3d_cuda_smem(
         x1, y1, z1, w1, x2, y2, z2, w2, _rbins_squared, result):
     start = cuda.grid(1)
@@ -19,13 +19,11 @@ def count_weighted_pairs_3d_cuda_smem(
     logminr = math.log(_rbins_squared[0]) / 2
 
     # putting rbins in local mem is the only thing that seemed to help here
-    rbins_squared = cuda.local.array(1024, numba.float32)
-    for i in range(nbins+1):
-        rbins_squared[i] = _rbins_squared[i]
+    rbins_squared = cuda.shared.array(1024, numba.float32)
+    if cuda.threadIdx.x == 0:
+        for i in range(nbins+1):
+            rbins_squared[i] = _rbins_squared[i]
 
-    lmem = cuda.local.array(1024, numba.float32)
-    for i in range(1024):
-        lmem[i] = 0
     smem = cuda.shared.array(1024, numba.float32)
     if cuda.threadIdx.x == 0:
         for i in range(1024):
@@ -42,11 +40,9 @@ def count_weighted_pairs_3d_cuda_smem(
 
             k = int((math.log(dsq)/2 - logminr) / dlogr)
             if k >= 0 and k < nbins:
-                lmem[k] += wp
+                cuda.atomic.add(smem, k, wp)
 
-    for k in range(nbins):
-        cuda.atomic.add(smem, k, lmem[k])
     cuda.syncthreads()
     if cuda.threadIdx.x == 0:
         for k in range(nbins):
-            cuda.atomic.add(result, k, lmem[k])
+            cuda.atomic.add(result, k, smem[k])
