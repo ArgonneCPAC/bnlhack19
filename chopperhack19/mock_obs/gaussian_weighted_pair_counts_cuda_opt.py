3b4a8ab88688ef6b2ef2cf1c02d38477ec9fee26
Matthew R Becker <becker.mr@gmail.com>
2019-09-24 17:45:21 -0400

    really stupid

--- chopperhack19/mock_obs/gaussian_weighted_pair_counts_cuda_opt.py
+++ chopperhack19/mock_obs/gaussian_weighted_pair_counts_cuda_opt.py
@@ -70,7 +70,7 @@ def count_weighted_pairs_3d_cuda_smem_noncuml(
 
 
 @cuda.jit(fastmath=True)
-def count_weighted_pairs_3d_cuda_smemload_noncuml(
+def count_weighted_pairs_3d_cuda_revchop_noncuml(
         x1, y1, z1, w1, x2, y2, z2, w2, _rbins_squared, result):
     start = cuda.grid(1)
     stride = cuda.gridsize(1)
@@ -82,27 +82,32 @@ def count_weighted_pairs_3d_cuda_smemload_noncuml(
         _rbins_squared[1] / _rbins_squared[0]) / 2
     logminr = math.log(_rbins_squared[0]) / 2
 
-    smem = cuda.shared.array(128, numba.float32)
-    if cuda.threadIdx.x == 0:
-        for i in range(128):
-            smem[i] = 0
-    cuda.syncthreads()
+    smem = cuda.shared.array(512, numba.float32)
+    smem[cuda.threadIdx.x] = 0
 
-    for i in range(start, n1, stride):
-        for j in range(n2):
-            dx = x1[i] - x2[j]
-            dy = y1[i] - y2[j]
-            dz = z1[i] - z2[j]
-            dsq = cuda.fma(dx, dx, cuda.fma(dy, dy, dz * dz))
+    for k in range(nbins):
+        for i in range(start, n1, stride):
+            for j in range(n2):
+                dx = x1[i] - x2[j]
+                dy = y1[i] - y2[j]
+                dz = z1[i] - z2[j]
+                dsq = cuda.fma(dx, dx, cuda.fma(dy, dy, dz * dz))
 
-            k = int((math.log(dsq)/2 - logminr) / dlogr)
-            if k >= 0 and k < nbins:
-                cuda.atomic.add(smem, k, w1[i] * w2[j])
+                _k = int((math.log(dsq)/2 - logminr) / dlogr)
+                if k == _k:
+                    smem[cuda.threadIdx.x] += (w1[i] * w2[j])
 
-    cuda.syncthreads()
-    if cuda.threadIdx.x == 0:
-        for k in range(nbins):
-            cuda.atomic.add(result, k, smem[k])
+        cuda.syncthreads()
+
+        i = cuda.blockDim // 2
+        while i > 0:
+            if cuda.threadIdx.x < i:
+                smem[cuda.threadIdx.x] += smem[cuda.threadIdx.x + i]
+            cuda.syncthreads()
+            i //= 2
+
+        if cuda.threadIdx.x == 0:
+            cuda.atomic.add(result, k, smem[0])
 
 
 @cuda.jit(fastmath=True)
