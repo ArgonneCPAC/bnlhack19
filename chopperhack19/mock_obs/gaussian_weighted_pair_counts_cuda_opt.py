c4e0cb617197bb3545f52fd7de09d81262658ab2
Matthew R Becker <becker.mr@gmail.com>
2019-09-24 00:10:12 -0400

    maybe this

--- chopperhack19/mock_obs/gaussian_weighted_pair_counts_cuda_opt.py
+++ chopperhack19/mock_obs/gaussian_weighted_pair_counts_cuda_opt.py
@@ -23,7 +23,7 @@ def count_weighted_pairs_3d_cuda_smem(
     # if cuda.threadIdx.x == 0:
     #     for i in range(1024):
     #         smem[i] = 0
-    cuda.syncthreads()
+    # cuda.syncthreads()
 
     for i in range(start, n1, stride):
         px = x1[i]
