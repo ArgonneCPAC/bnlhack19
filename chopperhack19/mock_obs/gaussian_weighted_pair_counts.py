16763298e726680be48c6e3612683a001ce8d9bd
Antonio Villarreal <asv13@icsubmit01.sdcc.bnl.gov>
2019-09-24 16:02:11 -0400

    preliminary mesh pair counting;

--- chopperhack19/mock_obs/gaussian_weighted_pair_counts.py
+++ chopperhack19/mock_obs/gaussian_weighted_pair_counts.py
@@ -7,40 +7,31 @@ import multiprocessing
 
 __all__ = (
     'count_weighted_pairs_3d_cuda',
-    'count_weighted_pairs_3d_launch_mesh',
+    'count_weighted_pairs_3d_cuda_mesh',
     'count_weighted_pairs_3d_cpu_corrfunc',
     'count_weighted_pairs_3d_cpu_mp',
     'count_weighted_pairs_3d_cpu')
 
-def count_weighted_pairs_3d_launch_mesh(
+@cuda.jit
+def count_weighted_pairs_3d_cuda_mesh(
         x1, y1, z1, w1, x2, y2, z2, w2, rbins_squared, result,
-        Lbox, blocks, threads):
-    from .chaining_mesh import calculate_chaining_mesh
-    """Implementation to count Npairs(<r), using chaining mesh methods.
+        nx, ny, nz):
+    """Naive pair counting with mesh in cuda. Note x/y/z/w are
+    the sorted array output by calculate_chained_mesh.
+    nx, ny, nz = mesh dimensions
     """
-    # first on the CPU we will be determining the mesh
-    nx, ny, nz = 5, 5, 5
-    lx, ly, lz = Lbox/nx, Lbox/ny, Lbox/nz
-    results = calculate_chaining_mesh(x1, y1, z1, Lbox, Lbox, Lbox, nx, ny, nz)
-    x1out, y1out, z1out, ixout, iyout, izout, cell_id_out, idx_sorted, cell_id_indices = results
-    results2 = calculate_chaining_mesh(x2, y2, z2, Lbox, Lbox, Lbox, nx, ny, nz)
-    x2out, y2out, z2out, ix2out, iy2out, iz2out, cell_id2_out, idx_sorted2, cell_id2_indices = results2
-    ncells = nx*ny*nz
-
-    for icell1 in range(ncells):
+    start = cuda.grid(1)
+    stride = cuda.gridsize(1)
+    
+    numcells = nx*ny*nz
+    for icell1 in range(start, numcells, stride):
         ifirst1 = cell_id_indices[icell1]
         ilast1 = cell_id_indices[icell1+1]
 
-        x_icell1 = x1out[ifirst1:ilast1]
-        y_icell1 = y1out[ifirst1:ilast1]
-        z_icell1 = z1out[ifirst1:ilast1]
-        # send these to CUDA
-        d_x1 = cuda.to_device(x_icell1)
-        d_y1 = cuda.to_device(y_icell1)
-        d_z1 = cuda.to_device(z_icell1)
-        # for dumb testing, let's just make x1 == w1
-        d_w1 = cuda.to_device(x_icell1)
-
+        x_icell1 = x1[ifirst1:ilast1]
+        y_icell1 = y1[ifirst1:ilast1]
+        z_icell1 = z1[ifirst1:ilast1]
+        w_icell1 = w1[wfirst1:wlast1]
         Ni = ilast1 - ifirst1
         if Ni > 0:
             ix1 = icell1 // (ny*nz)
@@ -63,28 +54,31 @@ def count_weighted_pairs_3d_launch_mesh(
                         ifirst2 = cell_id2_indices[icell2]
                         ilast2 = cell_id2_indices[icell2+1]
 
-                        x_icell2 = x2out[ifirst2:ilast2]
-                        y_icell2 = y2out[ifirst2:ilast2]
-                        z_icell2 = z2out[ifirst2:ilast2]
-                        # and let's send this to CUDA too
-                        d_x2 = cuda.to_device(x_icell2)
-                        d_y2 = cuda.to_device(y_icell2)
-                        d_z2 = cuda.to_device(z_icell2)
-                        # fake w again
-                        d_w2 = cuda.to_device(x_icell2)
-                        # and a smaller result
-                        miniresult = np.zeros(rbins_squared.shape[0]-1).astype(np.float32)
-                        d_miniresult = cuda.to_device(miniresult)
+                        x_icell2 = x2[ifirst2:ilast2]
+                        y_icell2 = y2[ifirst2:ilast2]
+                        z_icell2 = z2[ifirst2:ilast2]
+                        w_icell2 = w2[ifirst2:ilast2]
+
                         Nj = ilast2 - ifirst2
                         if Nj > 0:
-                            # at this point, we're looping over items - let's throw this at CUDA
-                            count_weighted_pairs_3d_cuda[blocks,threads](d_x1, d_y1, d_z1, 
-                                                         d_w1, d_x2, d_y2, d_z2,
-                                                         d_w2, rbins_squared, d_miniresult)
-                            # and once we're done, add that result 
-                            newresult = d_miniresult.copy_to_host()
-                            for k in range(rbins_squared.shape[0]-1):
-                                result[k] += newresult[k]
+                            for i in range(0, Ni):
+                                x1tmp = x_icell1[i]
+                                y1tmp = y_icell1[i]
+                                z1tmp = z_icell1[i] 
+                                w1tmp = w_icell1[i]
+                                for j in range(0, Nj):
+                                    #calculate the square distance
+                                    dx = x1tmp - x_icell2[j]
+                                    dy = y1tmp - y_icell2[j]
+                                    dz = z1tmp - z_icell2[j]
+                                    wprod = w1tmp*w_icell2[j]
+                                    dsq = dx*dx + dy*dy + dz*dz
+     
+                                    k = nbins-1
+                                    while dsq <= rbins_squared[k]:
+                                        cuda.atomic.add(result, k-1, wprod)
+                                        k=k-1
+                                        if k<0: break
 
 @cuda.jit
 def count_weighted_pairs_3d_cuda(
