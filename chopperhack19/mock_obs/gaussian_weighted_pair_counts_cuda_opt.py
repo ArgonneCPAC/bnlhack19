28cb3230f76643786b306d3ca8068a77d8b0c748
Matthew R Becker <becker.mr@gmail.com>
2019-09-24 06:36:21 -0400

    try this

--- chopperhack19/mock_obs/gaussian_weighted_pair_counts_cuda_opt.py
+++ chopperhack19/mock_obs/gaussian_weighted_pair_counts_cuda_opt.py
@@ -1,3 +1,4 @@
+import numpy as np
 import numba
 from numba import cuda
 import math
@@ -5,8 +6,15 @@ import math
 __all__ = ('count_weighted_pairs_3d_cuda_smem',)
 
 
-@cuda.jit
 def count_weighted_pairs_3d_cuda_smem(
+        x1, y1, z1, w1, x2, y2, z2, w2, rbins_squared, result):
+    res = _count_weighted_pairs_3d_cuda_smem(
+            x1, y1, z1, w1, x2, y2, z2, w2, rbins_squared, result)
+    return np.cumsum(res)
+
+
+@cuda.jit
+def _count_weighted_pairs_3d_cuda_smem(
         x1, y1, z1, w1, x2, y2, z2, w2, rbins_squared, result):
     start = cuda.grid(1)
     stride = cuda.gridsize(1)
