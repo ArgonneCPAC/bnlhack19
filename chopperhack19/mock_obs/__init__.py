cfa2983a15064db0d103b2bcf9894f0dea65adcd
Antonio Villarreal <asv13@icsubmit01.sdcc.bnl.gov>
2019-09-25 10:26:28 -0400

    not sure why this changed, but okay

--- chopperhack19/mock_obs/__init__.py
+++ chopperhack19/mock_obs/__init__.py
@@ -3,4 +3,5 @@ from .gaussian_weighted_histogram import *
 from .gaussian_weighted_pair_counts import *
 from .gaussian_weighted_pair_counts_cuda_opt import *
 from .chaining_mesh import *
+from .double_chop_kernel import *
 from .tests import *
