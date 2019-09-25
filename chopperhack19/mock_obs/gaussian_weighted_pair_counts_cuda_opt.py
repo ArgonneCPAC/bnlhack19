cd2fa0dfa47d893a37f62fbda596a401b60a9606
Matthew R. Becker <beckermr@users.noreply.github.com>
2019-09-25 15:18:55 -0400

    Update gaussian_weighted_pair_counts_cuda_opt.py

--- chopperhack19/mock_obs/gaussian_weighted_pair_counts_cuda_opt.py
+++ chopperhack19/mock_obs/gaussian_weighted_pair_counts_cuda_opt.py
@@ -71,67 +71,67 @@ def count_weighted_pairs_3d_cuda_smem_noncuml(
             cuda.atomic.add(result, k, smem[k])
 
 
-exec(jinja2.Template("""
-@cuda.jit(fastmath=True)
-def count_weighted_pairs_3d_cuda_revchop_noncuml(
-        x1, y1, z1, w1, x2, y2, z2, w2, _rbins_squared, result):
-    start = cuda.grid(1)
-    stride = cuda.gridsize(1)
+# exec(jinja2.Template("""
+# @cuda.jit(fastmath=True)
+# def count_weighted_pairs_3d_cuda_revchop_noncuml(
+#         x1, y1, z1, w1, x2, y2, z2, w2, _rbins_squared, result):
+#     start = cuda.grid(1)
+#     stride = cuda.gridsize(1)
 
-    n1 = x1.shape[0]
-    n2 = x2.shape[0]
-    nbins = _rbins_squared.shape[0]-1
-    dlogr = math.log(
-        _rbins_squared[1] / _rbins_squared[0]) / 2
-    logminr = math.log(_rbins_squared[0]) / 2
+#     n1 = x1.shape[0]
+#     n2 = x2.shape[0]
+#     nbins = _rbins_squared.shape[0]-1
+#     dlogr = math.log(
+#         _rbins_squared[1] / _rbins_squared[0]) / 2
+#     logminr = math.log(_rbins_squared[0]) / 2
 
-    smem = cuda.shared.array(512, numba.float32)
+#     smem = cuda.shared.array(512, numba.float32)
 
-    {% for bin in range({{ n_bins }}) %}
-    g{{ bin }} = 0
-    {% endfor %}
-    for i in range(start, n1, stride):
-        for j in range(n2):
-            dx = x1[i] - x2[j]
-            dy = y1[i] - y2[j]
-            dz = z1[i] - z2[j]
-            dsq = cuda.fma(dx, dx, cuda.fma(dy, dy, dz * dz))
+#     {% for bin in range({{ n_bins }}) %}
+#     g{{ bin }} = 0
+#     {% endfor %}
+#     for i in range(start, n1, stride):
+#         for j in range(n2):
+#             dx = x1[i] - x2[j]
+#             dy = y1[i] - y2[j]
+#             dz = z1[i] - z2[j]
+#             dsq = cuda.fma(dx, dx, cuda.fma(dy, dy, dz * dz))
 
-            k = int((math.log(dsq)/2 - logminr) / dlogr)
-            if k == 0:
-                g0 += (w1[i] * w2[j])
-            {% for bin in range(1, 16) %}
-            elif k == {{ bin }}:
-                g{{ bin }} += (w1[i] * w2[j])
-            {% endfor %}
+#             k = int((math.log(dsq)/2 - logminr) / dlogr)
+#             if k == 0:
+#                 g0 += (w1[i] * w2[j])
+#             {% for bin in range(1, 16) %}
+#             elif k == {{ bin }}:
+#                 g{{ bin }} += (w1[i] * w2[j])
+#             {% endfor %}
 
-    for k in range({{ n_bins }}):
-        if k == 0:
-            smem[cuda.threadIdx.x] = g0
-        {% for bin in range(1, {{ n_bins }}) %}
-        elif k == {{ bin }}:
-            smem[cuda.threadIdx.x] = g{{ bin }}
-        {% endfor %}
-        cuda.syncthreads()
+#     for k in range({{ n_bins }}):
+#         if k == 0:
+#             smem[cuda.threadIdx.x] = g0
+#         {% for bin in range(1, {{ n_bins }}) %}
+#         elif k == {{ bin }}:
+#             smem[cuda.threadIdx.x] = g{{ bin }}
+#         {% endfor %}
+#         cuda.syncthreads()
 
-        i = numba.int32(cuda.blockDim.x) // 2
-        while i > 32:
-            if cuda.threadIdx.x < i:
-                smem[cuda.threadIdx.x] += smem[cuda.threadIdx.x + i]
-            cuda.syncthreads()
-            i = i >> 1
+#         i = numba.int32(cuda.blockDim.x) // 2
+#         while i > 32:
+#             if cuda.threadIdx.x < i:
+#                 smem[cuda.threadIdx.x] += smem[cuda.threadIdx.x + i]
+#             cuda.syncthreads()
+#             i = i >> 1
 
-        if cuda.threadIdx.x < 32:
-            smem[cuda.threadIdx.x] += smem[cuda.threadIdx.x + 32]
-            smem[cuda.threadIdx.x] += smem[cuda.threadIdx.x + 16]
-            smem[cuda.threadIdx.x] += smem[cuda.threadIdx.x + 8]
-            smem[cuda.threadIdx.x] += smem[cuda.threadIdx.x + 4]
-            smem[cuda.threadIdx.x] += smem[cuda.threadIdx.x + 2]
-            smem[cuda.threadIdx.x] += smem[cuda.threadIdx.x + 1]
+#         if cuda.threadIdx.x < 32:
+#             smem[cuda.threadIdx.x] += smem[cuda.threadIdx.x + 32]
+#             smem[cuda.threadIdx.x] += smem[cuda.threadIdx.x + 16]
+#             smem[cuda.threadIdx.x] += smem[cuda.threadIdx.x + 8]
+#             smem[cuda.threadIdx.x] += smem[cuda.threadIdx.x + 4]
+#             smem[cuda.threadIdx.x] += smem[cuda.threadIdx.x + 2]
+#             smem[cuda.threadIdx.x] += smem[cuda.threadIdx.x + 1]
 
-        if cuda.threadIdx.x == 0:
-            cuda.atomic.add(result, k, smem[0])
-""").render(n_bins=2))
+#         if cuda.threadIdx.x == 0:
+#             cuda.atomic.add(result, k, smem[0])
+# """).render(n_bins=2))
 
 
 @cuda.jit(fastmath=True)
