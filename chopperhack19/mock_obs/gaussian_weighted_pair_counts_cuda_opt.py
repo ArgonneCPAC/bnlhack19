ad961282da9235c5ea465fdfcc4d2dc85fb61117
Matthew R Becker <becker.mr@gmail.com>
2019-09-25 08:09:14 -0400

    try 2d block decomp

--- chopperhack19/mock_obs/gaussian_weighted_pair_counts_cuda_opt.py
+++ chopperhack19/mock_obs/gaussian_weighted_pair_counts_cuda_opt.py
@@ -8,7 +8,8 @@ __all__ = (  # noqa
     'count_weighted_pairs_3d_cuda_noncuml',
     'count_weighted_pairs_3d_cuda_transpose_noncuml',
     'count_weighted_pairs_3d_cuda_revchop_noncuml',
-    'count_weighted_pairs_3d_cuda_noncuml')
+    'count_weighted_pairs_3d_cuda_noncuml',
+    'count_weighted_pairs_3d_cuda_2d_noncuml')
 
 
 @cuda.jit(fastmath=True)
@@ -56,6 +57,38 @@ def count_weighted_pairs_3d_cuda_smem_noncuml(
 
     for i in range(start, n1, stride):
         for j in range(n2):
+            dx = x1[i] - x2[j]
+            dy = y1[i] - y2[j]
+            dz = z1[i] - z2[j]
+            dsq = cuda.fma(dx, dx, cuda.fma(dy, dy, dz * dz))
+
+            k = int((math.log(dsq)/2 - logminr) / dlogr)
+            if k >= 0 and k < nbins:
+                cuda.atomic.add(smem, k, w1[i] * w2[j])
+
+    cuda.syncthreads()
+    if cuda.threadIdx.x == 0:
+        for k in range(nbins):
+            cuda.atomic.add(result, k, smem[k])
+
+
+@cuda.jit(fastmath=True)
+def count_weighted_pairs_3d_cuda_2d_noncuml(
+        x1, y1, z1, w1, x2, y2, z2, w2, _rbins_squared, result):
+    starti, startj = cuda.grid(2)
+    stridei, stridej = cuda.gridsize(2)
+
+    n1 = x1.shape[0]
+    n2 = x2.shape[0]
+    nbins = _rbins_squared.shape[0]-1
+    dlogr = math.log(
+        _rbins_squared[1] / _rbins_squared[0]) / 2
+    logminr = math.log(_rbins_squared[0]) / 2
+
+    smem = cuda.shared.array(128, numba.float32)
+
+    for i in range(starti, n1, stridei):
+        for j in range(startj, n2, stridej):
             dx = x1[i] - x2[j]
             dy = y1[i] - y2[j]
             dz = z1[i] - z2[j]
--- scripts/master_timing_test.py
+++ scripts/master_timing_test.py
@@ -6,7 +6,7 @@ from chopperhack19.mock_obs.tests import random_weighted_points
 from chopperhack19.mock_obs.tests.generate_test_data import (
     DEFAULT_RBINS_SQUARED, DEFAULT_NMESH)
 from time import time
-from numba import int32
+
 
 @click.command()
 @click.option('--func', default='count_weighted_pairs_3d_cpu_serial',
@@ -30,6 +30,9 @@ def _main(func, blocks, threads, npoints):
         print('blocks:', blocks)
         print('threads:', threads)
 
+        blocks_2d = (16, 32)
+        threads_2d = (16, 32)
+
     Lbox = 1000.
     result = np.zeros_like(DEFAULT_RBINS_SQUARED)[:-1]
     result = result.astype(np.float32)
@@ -44,16 +47,16 @@ def _main(func, blocks, threads, npoints):
         _cell_id2_indices = np.zeros(len(_x1))
         _ndiv = np.array([DEFAULT_NMESH]*3, dtype=np.int32)
         _num_cell2_steps = np.array([1]*3, dtype=np.int32)
-        func[blocks,threads](
+        func[blocks, threads](
             _x1, _y1, _z1, _w1, _x2, _y2, _z2, _w2,
             DEFAULT_RBINS_SQUARED, result,
             _ndiv, _cell_id_indices, _cell_id2_indices,
             _num_cell2_steps)
-    elif 'cuda' in func_str and 'transpose' not in func_str:
-        func[blocks, threads](
+    elif 'cuda_2d' in func_str:
+        func[blocks_2d, threads_2d](
             _x1, _y1, _z1, _w1, _x2, _y2, _z2, _w2,
             DEFAULT_RBINS_SQUARED, result)
-    elif 'cuda' in func_str and 'transpose' in func_str:
+    elif 'cuda_transpose' in func_str:
         ptswts1 = np.stack(
             [_x1, _y1, _z1, _w1], axis=1).ravel().astype(np.float32)
         ptswts2 = np.stack(
@@ -61,6 +64,10 @@ def _main(func, blocks, threads, npoints):
 
         func[blocks, threads](
             ptswts1, ptswts2, DEFAULT_RBINS_SQUARED, result)
+    elif 'cuda' in func_str:
+        func[blocks, threads](
+            _x1, _y1, _z1, _w1, _x2, _y2, _z2, _w2,
+            DEFAULT_RBINS_SQUARED, result)
     else:
         func(
             _x1, _y1, _z1, _w1, _x2, _y2, _z2, _w2,
@@ -70,21 +77,26 @@ def _main(func, blocks, threads, npoints):
     n2 = npoints
     x1, y1, z1, w1 = random_weighted_points(n1, Lbox, 0)
     x2, y2, z2, w2 = random_weighted_points(n2, Lbox, 1)
+
     if 'cuda_mesh' in func_str:
         from chopperhack19.mock_obs import chaining_mesh as cm
         nx = DEFAULT_NMESH
         ny = DEFAULT_NMESH
         nz = DEFAULT_NMESH
-        results = cm.calculate_chaining_mesh(x1, y1, z1, w1, Lbox, Lbox, Lbox, nx, ny, nz)
-        x1out, y1out, z1out, w1out, ixout, iyout, izout, cell_id_out, idx_sorted, cell_id_indices = results
-        results2 = cm.calculate_chaining_mesh(x2, y2, z2, w2, Lbox, Lbox, Lbox, nx, ny, nz)
-        x2out, y2out, z2out, w2out, ix2out, iy2out, iz2out, cell_id2_out, idx_sorted2, cell_id2_indices = results2
+        results = cm.calculate_chaining_mesh(
+            x1, y1, z1, w1, Lbox, Lbox, Lbox, nx, ny, nz)
+        (x1out, y1out, z1out, w1out, ixout, iyout, izout,
+         cell_id_out, idx_sorted, cell_id_indices) = results
+        results2 = cm.calculate_chaining_mesh(
+            x2, y2, z2, w2, Lbox, Lbox, Lbox, nx, ny, nz)
+        (x2out, y2out, z2out, w2out, ix2out, iy2out, iz2out,
+         cell_id2_out, idx_sorted2, cell_id2_indices) = results2
 
         d_x1 = cuda.to_device(x1out.astype(np.float32))
         d_y1 = cuda.to_device(y1out.astype(np.float32))
         d_z1 = cuda.to_device(z1out.astype(np.float32))
         d_w1 = cuda.to_device(w1out.astype(np.float32))
-      
+
         d_x2 = cuda.to_device(x2.astype(np.float32))
         d_y2 = cuda.to_device(y2.astype(np.float32))
         d_z2 = cuda.to_device(z2.astype(np.float32))
@@ -108,7 +120,52 @@ def _main(func, blocks, threads, npoints):
         end = time()
         assert np.all(np.isfinite(results_host))
         runtime = (end-start)/3
-    elif 'cuda' in func_str and 'transpose' not in func_str:
+    elif 'cuda_2d' in func_str:
+        d_x1 = cuda.to_device(x1.astype(np.float32))
+        d_y1 = cuda.to_device(y1.astype(np.float32))
+        d_z1 = cuda.to_device(z1.astype(np.float32))
+        d_w1 = cuda.to_device(w1.astype(np.float32))
+
+        d_x2 = cuda.to_device(x2.astype(np.float32))
+        d_y2 = cuda.to_device(y2.astype(np.float32))
+        d_z2 = cuda.to_device(z2.astype(np.float32))
+        d_w2 = cuda.to_device(w2.astype(np.float32))
+
+        d_rbins_squared = cuda.to_device(
+            DEFAULT_RBINS_SQUARED.astype(np.float32))
+        d_result = cuda.device_array_like(result)
+
+        start = time()
+        for _ in range(3):
+            func[blocks_2d, threads_2d](
+                d_x1, d_y1, d_z1, d_w1, d_x2, d_y2, d_z2, d_w2,
+                d_rbins_squared, d_result)
+            results_host = d_result.copy_to_host()
+        end = time()
+        assert np.all(np.isfinite(results_host))
+        runtime = (end-start)/3
+
+    elif 'cuda_transpose' in func_str:
+        ptswts1 = np.stack([x1, y1, z1, w1], axis=1).ravel().astype(np.float32)
+        ptswts2 = np.stack([x2, y2, z2, w2], axis=1).ravel().astype(np.float32)
+
+        d_ptswts1 = cuda.to_device(ptswts1)
+        d_ptswts2 = cuda.to_device(ptswts2)
+
+        d_rbins_squared = cuda.to_device(
+            DEFAULT_RBINS_SQUARED.astype(np.float32))
+        d_result = cuda.device_array_like(result)
+
+        start = time()
+        for _ in range(3):
+            func[blocks, threads](
+                d_ptswts1, d_ptswts2, d_rbins_squared, d_result)
+            results_host = d_result.copy_to_host()
+        end = time()
+        assert np.all(np.isfinite(results_host))
+        runtime = (end-start)/3
+
+    elif 'cuda' in func_str:
         d_x1 = cuda.to_device(x1.astype(np.float32))
         d_y1 = cuda.to_device(y1.astype(np.float32))
         d_z1 = cuda.to_device(z1.astype(np.float32))
@@ -132,25 +189,7 @@ def _main(func, blocks, threads, npoints):
         end = time()
         assert np.all(np.isfinite(results_host))
         runtime = (end-start)/3
-    elif 'cuda' in func_str and 'transpose' in func_str:
-        ptswts1 = np.stack([x1, y1, z1, w1], axis=1).ravel().astype(np.float32)
-        ptswts2 = np.stack([x2, y2, z2, w2], axis=1).ravel().astype(np.float32)
 
-        d_ptswts1 = cuda.to_device(ptswts1)
-        d_ptswts2 = cuda.to_device(ptswts2)
-
-        d_rbins_squared = cuda.to_device(
-            DEFAULT_RBINS_SQUARED.astype(np.float32))
-        d_result = cuda.device_array_like(result)
-
-        start = time()
-        for _ in range(3):
-            func[blocks, threads](
-                d_ptswts1, d_ptswts2, d_rbins_squared, d_result)
-            results_host = d_result.copy_to_host()
-        end = time()
-        assert np.all(np.isfinite(results_host))
-        runtime = (end-start)/3
     else:
         d_x1 = x1
         d_y1 = y1
