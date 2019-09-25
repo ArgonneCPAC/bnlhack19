779b6dc93f6cb9d60d2b6ec8a129cb2e41f2c1c9
Matthew R Becker <becker.mr@gmail.com>
2019-09-25 14:23:54 -0400

    time pairs

--- chopperhack19/mock_obs/gaussian_weighted_pair_counts_cuda_opt.py
+++ chopperhack19/mock_obs/gaussian_weighted_pair_counts_cuda_opt.py
@@ -36,6 +36,30 @@ def count_weighted_pairs_3d_cuda_noncuml(
                 cuda.atomic.add(result, k, w1[i] * w2[j])
 
 
+@cuda.jit(fastmath=True)
+def count_weighted_pairs_3d_cuda_noncuml_pairsonly(
+        x1, y1, z1, w1, x2, y2, z2, w2, _rbins_squared, result):
+    start = cuda.grid(1)
+    stride = cuda.gridsize(1)
+
+    n1 = x1.shape[0]
+    n2 = x2.shape[0]
+    nbins = _rbins_squared.shape[0]-1
+    dlogr = math.log(_rbins_squared[1] / _rbins_squared[0]) / 2
+    logminr = math.log(_rbins_squared[0]) / 2
+
+    for i in range(start, n1, stride):
+        for j in range(n2):
+            dx = x1[i] - x2[j]
+            dy = y1[i] - y2[j]
+            dz = z1[i] - z2[j]
+            dsq = cuda.fma(dx, dx, cuda.fma(dy, dy, dz * dz))
+
+            k = int((math.log(dsq)/2 - logminr) / dlogr)
+            if k >= 0 and k < nbins:
+                result[k] += (w1[i] * w2[j])
+
+
 SMEM_CHUNK_SIZE = 2048
 
 
