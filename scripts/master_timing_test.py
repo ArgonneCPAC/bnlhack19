ea05cb493172e3bdf4558c909ff4b8d70dd92b6e
Matthew R Becker <becker.mr@gmail.com>
2019-09-26 12:58:10 -0400

    again

--- scripts/master_timing_test.py
+++ scripts/master_timing_test.py
@@ -374,7 +374,14 @@ def _main(func, blocks, threads, npoints, nmesh1, nmesh2, skip_numba_comp):
         runtime = (end-start)/3
         results_host = d_result
 
+    # run corrfunc to check
+    result_test = np.zeros_like(result)
+    chopperhack19.mock_obs.count_weighted_pairs_3d_cpu_corrfunc(
+        x1, y1, z1, w1, x2, y2, z2, w2, DEFAULT_RBINS_SQUARED, result_test)
+    correct = np.allclose(result_test, results_host)
+
     print('time:', runtime)
+    print('correct (only true for cumulative bins):', correct)
     print('result:', results_host)
 
 
