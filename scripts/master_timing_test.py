e51bdeb94d228fa68a0b76a80822c18de6a77cb8
Matthew R Becker <becker.mr@gmail.com>
2019-09-26 09:55:13 -0400

    int casts

--- scripts/master_timing_test.py
+++ scripts/master_timing_test.py
@@ -109,10 +109,16 @@ def _main(func, blocks, threads, npoints):
             x2out, y2out, z2out, w2out, indx2,
             DEFAULT_RBINS_SQUARED, result)
     elif 'cuda_transpose2d' in func_str:
-        ptswts1 = np.stack(
-            [_x1, _y1, _z1, _w1], axis=1).ravel().astype(np.float32)
-        ptswts2 = np.stack(
-            [_x2, _y2, _z2, _w2], axis=1).ravel().astype(np.float32)
+        ptswts1 = np.empty((x1out.size, 4), dtype=np.float32)
+        ptswts1[:, 0] = x1out
+        ptswts1[:, 1] = y1out
+        ptswts1[:, 2] = z1out
+        ptswts1[:, 3] = w1out
+        ptswts2 = np.empty((x2out.size, 4), dtype=np.float32)
+        ptswts2[:, 0] = x2out
+        ptswts2[:, 1] = y2out
+        ptswts2[:, 2] = z2out
+        ptswts2[:, 3] = w2out
 
         func[(blocks, blocks), (32, 32)](
             ptswts1, ptswts2, DEFAULT_RBINS_SQUARED, result)
@@ -270,8 +276,16 @@ def _main(func, blocks, threads, npoints):
         end = time()
         runtime = (end-start)/3
     elif 'cuda_transpose2d' in func_str:
-        ptswts1 = np.stack([x1, y1, z1, w1], axis=1).ravel().astype(np.float32)
-        ptswts2 = np.stack([x2, y2, z2, w2], axis=1).ravel().astype(np.float32)
+        ptswts1 = np.empty((x1out.size, 4), dtype=np.float32)
+        ptswts1[:, 0] = x1out
+        ptswts1[:, 1] = y1out
+        ptswts1[:, 2] = z1out
+        ptswts1[:, 3] = w1out
+        ptswts2 = np.empty((x2out.size, 4), dtype=np.float32)
+        ptswts2[:, 0] = x2out
+        ptswts2[:, 1] = y2out
+        ptswts2[:, 2] = z2out
+        ptswts2[:, 3] = w2out
 
         d_ptswts1 = cuda.to_device(ptswts1)
         d_ptswts2 = cuda.to_device(ptswts2)
